f3e7836b0375648ba02848c58b96b447ada8e46f
watermelon <yuanguo.h001@gmail.com>
2025-01-07 11:27:38 +0800

    formatting

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        local lsp_conf = require("lspconfig")
-        local lsp_util = require("lspconfig.util")
-        local user_home = os.getenv("HOME")
+        local lsp_conf     = require("lspconfig")
+        local lsp_util     = require("lspconfig.util")
+        local user_home    = os.getenv("HOME")
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
         local ccls_opts = {
             cmd = {'ccls', '--log-file=' .. user_home .. '/ccls/log'},
@@ -18,9 +19,9 @@ return {
             -- ccls does not support sending a null root directory
             single_file_support = false,
 
-			root_dir = function(fname)
-				return lsp_util.root_pattern('compile_commands.json', '.ccls')(fname) or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
-			end,
+            root_dir = function(fname)
+                return lsp_util.root_pattern('compile_commands.json', '.ccls')(fname) or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+            end,
 
             init_options = {
                 compilationDatabaseDirectory = "build",
@@ -34,9 +35,10 @@ return {
                     directory = user_home .. '/ccls/cache',
                 },
             },
+
+            capabilities = capabilities,
         }
 
         lsp_conf.ccls.setup(ccls_opts)
     end
-
 }
